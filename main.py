4f3347a9164b4ddecefeb4c6b48ba425056df3c9
MechiH <65371921+MechiH@users.noreply.github.com>
2021-04-24 01:27:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ server =smtplib.SMTP_SSL('smtp.gmail.com', 465) # define the server
 
 server.ehlo() # start the server 
 
-server.login('houssemmch21@gmail.com','jxqnyyzglzxfdnro') # login to your account
+server.login('YOUR_EMAIL','YOUR_PASSWORD') # login to your account
 
 msg = MIMEMultipart()
 msg['From']='HoussemMechi'
-msg['To']='houssem.mch@hotmail.com'
+msg['To']='RECEIVER_EMAIL'
 msg['Subject']= 'Test From My ClientMailing'
 
-with open('/home/housemmch/Desktop/workspace/Python/MailingClient/msg.txt','r') as f :
+with open('URL_TO_TEXT/msg.txt','r') as f :
     message = f.read()
 msg.attach(MIMEText(message,'plain')) # we are not adding the text as an attachement but as a body
 
-filename = '/home/housemmch/Desktop/workspace/Python/MailingClient/code.png'
+filename = 'URL_TO_IMAGE/code.png'
 
 attachement=open(filename ,'rb')# rb = readbinary
 
@@ -29,8 +29,8 @@ p=MIMEBase('application','octet-stream')
 p.set_payload(attachement.read())
 
 encoders.encode_base64(p) #encode image
-p.add_header('Content-Disposition',f'attachement; filename={filename}')# add a header tp p
+p.add_header('Content-Disposition',f'attachement; filename={filename}')# add a header to p
 msg.attach(p) # add the payload to the message
 
 text=msg.as_string()
-server.sendmail('houssemmch21@gmail.com','houssem.mch@hotmail.com',text)
+server.sendmail('YOUR_EMAIL','RECEIVER_EMAIL',text)
